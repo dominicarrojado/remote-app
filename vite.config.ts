b6d83146a15513ee2731f89d3f3dbdfab1f7bfb1
Dominic Arrojado <dominicarrojado@gmail.com>
2025-03-22 08:31:44 +0800

    make app as remote using module federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { federation } from '@module-federation/vite';
+import { dependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: 'chrome89',
+  },
+  plugins: [
+    federation({
+      filename: 'remoteEntry.js',
+      name: 'remote',
+      exposes: {
+        './remote-app': './src/App.tsx',
+      },
+      remotes: {},
+      shared: {
+        react: {
+          requiredVersion: dependencies.react,
+          singleton: true,
+        },
+      },
+    }),
+    react()
+  ],
 })
